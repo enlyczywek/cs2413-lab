b18258564a4a19035c5b20febb19e6ff55dc42ce
enlyczywek <enlyczywek@gmail.com>
2026-02-20 11:26:50 -0600

    Update Question2 student.c

--- Assignment1/Question2/student.c
+++ Assignment1/Question2/student.c
@@ -17,6 +17,18 @@
 int majorityElement(int* nums, int numsSize) {
      // TODO: implement
 
+     int count = 0;
+
+     for (int i = 0; i < numsSize; i++) {
+          for (int j = i + 1; j < numsSize; j++){
+               if (nums[i] == nums[j]) {
+                    count++;
+               }
+          }
+          if (count >= numsSize/2) return nums[i];
+         
+     }
+     return 0;
 
 
 }
